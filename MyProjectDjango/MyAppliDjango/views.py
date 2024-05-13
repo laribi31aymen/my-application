49468c094b61c25c3ee20b095a3fa3bc2462cf0f
laribi31aymen <168308448+laribi31aymen@users.noreply.github.com>
2024-05-13 11:04:41 +0200

    Update views.py

--- MyProjectDjango/MyAppliDjango/views.py
+++ MyProjectDjango/MyAppliDjango/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import laribi_amghar
-def mon_endpoint(request):
+from laribi_amghar import *
+def ma_vue(request):
+
     resultat = laribi_amghar
     return HttpResponse(resultat)
 # Create your views here.
