4793ba61584ae33bc45c6b03587ab31ddc06f2c4
laribi31aymen <168308448+laribi31aymen@users.noreply.github.com>
2024-05-20 10:29:19 +0200

    t
    template

--- MyProjectDjango/MyAppliDjango/views.py
+++ MyProjectDjango/MyAppliDjango/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from laribi_amghar import *
-def ma_vue(request):
 
-    resultat = laribi_amghar
-    return HttpResponse(resultat)
-# Create your views here.
+# views.py
+from django.shortcuts import render
+import cv2
+import mediapipe as mp
+
+def hand_detection_view(request):
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                cv2.circle(frame, (int(hand_landmarks.landmark[0].x * image.shape[1]), int(hand_landmarks.landmark[0].y * image.shape[0]), 10, (0, 255, 0), -1))
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame_data = jpeg.tobytes()
+
+        return render(request, 'hand_detection.html', {'frame_data': frame_data})
+
+    cap.release()
+    cv2.destroyAllWindows()
